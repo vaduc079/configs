98c847fc0a9d5c3ce3fd3fcb60ee710aa1972423
ducva <ducvuanh.79@gmail.com>
2024-06-25 00:53:21 +0700

    feat(sway, foot, fuzzel): add 4k output + clamshell config, enable dpi-aware
    - Enable dpi-aware for foot and fuzzle
    - Sway: add 4k output configuration, clamshell support, move workspace
    between ouputs key binds

---
+++ .config/sway/scripts/clamshell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+if [ -d "/proc/acpi" ]; then
+  if cat /proc/acpi/button/lid/*/state | grep -q open; then
+    swaymsg output eDP-1 enable
+  else
+    swaymsg output eDP-1 disable
+  fi
+fi
