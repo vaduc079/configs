24d4f59ba1fee5df295e05cdf7ab576bb893b2f5
Duc Vu <duc.vu@shopback.com>
2026-03-22 22:36:37 +0700

    chore(finicky): update config

--- .config/finicky.ts
+++ .config/finicky.ts
@@ -2,16 +2,17 @@
 import type { FinickyConfig } from "/Applications/Finicky.app/Contents/Resources/finicky.d.ts";
 
 function shouldRedirectToChrome(hostname: string): boolean {
-  return hostname.includes("figma.com") ||
+  return (
+    hostname.includes("figma.com") ||
     hostname.includes("cursor.com") ||
-    hostname.includes(".openai.com") ||
     hostname.includes("mcp.atlassian.com") ||
     hostname.includes("https://slack.com") ||
     hostname.includes(".slack.com") ||
     hostname.includes("shopback.slack.com") ||
     // hostname.includes("console.jumpcloud.com") ||
     hostname.includes(".zscaler.com") ||
-    hostname.includes("n8n.svc.shopback.com");
+    hostname.includes("n8n.svc.shopback.com")
+  );
 }
 
 export default {
@@ -23,16 +24,18 @@ export default {
   handlers: [
     {
       match: (url) => {
-        const isGoogleRedirect = url.href.startsWith("https://www.google.com/url?q=")
+        const isGoogleRedirect = url.href.startsWith(
+          "https://www.google.com/url?q=",
+        );
         if (isGoogleRedirect) {
-          const redirectUrlStr = url.searchParams.get('q')
+          const redirectUrlStr = url.searchParams.get("q");
           if (!redirectUrlStr) return false;
 
           const redirectURL = new URL(redirectUrlStr);
-          return shouldRedirectToChrome(redirectURL.hostname)
+          return shouldRedirectToChrome(redirectURL.hostname);
         }
 
-        return shouldRedirectToChrome(url.hostname)
+        return shouldRedirectToChrome(url.hostname);
       },
       browser: "com.google.Chrome",
     },
