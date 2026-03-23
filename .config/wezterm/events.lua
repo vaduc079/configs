2534c271ecc60c54d692aed5803018c552c27acc
Duc Vu <duc.vu@shopback.com>
2026-03-23 22:33:33 +0700

    chore: update config

--- .config/wezterm/events.lua
+++ .config/wezterm/events.lua
@@ -4,22 +4,22 @@ local M = {}
 
 function M.apply_to_config(_config)
 	-- Open wezterm at center of screen
-	wezterm.on("gui-startup", function(cmd)
-		local screen = wezterm.gui.screens().main
-		local ratio = 1
-		local width = math.floor(screen.width * ratio)
-		local height = math.floor(screen.height * ratio)
-
-		cmd = cmd or {}
-		cmd.position = {
-			x = math.floor((screen.width - width) / 2),
-			y = math.floor((screen.height - height) / 2),
-			origin = "MainScreen",
-		}
-
-		local _tab, _pane, window = wezterm.mux.spawn_window(cmd)
-		window:gui_window():set_inner_size(width, height)
-	end)
+	-- wezterm.on("gui-startup", function(cmd)
+	-- 	local screen = wezterm.gui.screens().main
+	-- 	local ratio = 1
+	-- 	local width = math.floor(screen.width * ratio)
+	-- 	local height = math.floor(screen.height * ratio)
+	--
+	-- 	cmd = cmd or {}
+	-- 	cmd.position = {
+	-- 		x = math.floor((screen.width - width) / 2),
+	-- 		y = math.floor((screen.height - height) / 2),
+	-- 		origin = "MainScreen",
+	-- 	}
+	--
+	-- 	local _tab, _pane, window = wezterm.mux.spawn_window(cmd)
+	-- 	window:gui_window():set_inner_size(width, height)
+	-- end)
 
 	-- Show which key table is active in the status area
 	wezterm.on("update-right-status", function(window, _pane)
--- .config/wezterm/options.lua
+++ .config/wezterm/options.lua
@@ -40,7 +40,7 @@ function M.apply_to_config(config)
 	config.tab_max_width = 50
 
 	config.window_decorations = "RESIZE"
-	config.window_background_opacity = 0.95
+	config.window_background_opacity = 0.98
 	config.macos_window_background_blur = 15
 
 	config.pane_focus_follows_mouse = true
